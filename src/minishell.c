5942a0b339414c35dacd365abeae7b96ef53d491
Viacheslav Moroz <vmoroz@c3r3s4.42prague.com>
2024-12-23 14:49:31 +0100

    id added

--- src/minishell.c
+++ src/minishell.c
@@ -3,23 +3,41 @@
 int	main(int argc, char **argv, char **env)
 {
 	t_prompt	prompt;
+	pid_t		id;
+
 	(void)argc;
 	(void)argv;
-	(void)env;
-
-	setup_handlers();
 	
+	setup_handlers();
 	while (1)
 	{
 		prompt.input = ft_prompt();
-		lexer(prompt.input);
-		if (prompt.input == NULL)
+		id = fork();
+		if (id == -1)
 		{
-			printf("vpizdu....\n");
+			perror("no fork today");
 			break ;
 		}
-		add_history(prompt.input);
-		handle_input(&prompt, env);
+		if (id == 0)
+		{
+			lexer(prompt.input);
+			if (prompt.input == NULL)
+			{
+				printf("vpizdu....\n");
+				break ;
+			}
+			add_history(prompt.input);
+			handle_input(&prompt, env);
+			exit(0);
+		}
+		else
+		{
+			add_history(prompt.input);
+			waitpid(id, NULL, 0);
+			printf("%d\n",id);
+			printf("ZzZzZz...\n");
+		}
 	}
+	free(prompt.input);
 	return (0);
 }
